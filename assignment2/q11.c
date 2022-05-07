ee11221c99fe553be81f07a194f964135039a1c5
AuenKr <103646877+AuenKr@users.noreply.github.com>
2022-05-07 11:27:34 +0530

    Update q11.c

--- assignment2/q11.c
+++ assignment2/q11.c
@@ -10,10 +10,10 @@ int main()
     printf("Enter 4 numbers:\n");
     scanf("%d %d %d %d", &a, &b, &c, &d);
 
-    if (a >= b & a >= c & a >= d)
+    if (a >= b && a >= c && a >= d)
         printf("%d is greatest", a);
 
-    else if (b >= c & b >= d)
+    else if (b >= c && b >= d)
         printf("%d is greatest", b);
 
     else if (c >= d)
