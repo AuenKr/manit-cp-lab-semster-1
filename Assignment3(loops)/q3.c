b644aaa8c881904182e0bff3c8b40c8be4b9a69e
AuenKr <103646877+AuenKr@users.noreply.github.com>
2022-05-11 08:59:03 +0530

    Update q3.c

--- Assignment3(loops)/q3.c
+++ Assignment3(loops)/q3.c
@@ -14,6 +14,6 @@ int main()
         if (i % 7 == 0 && i % 63 != 0)
             sum += i;
     }
-    printf("Sum of integers between 9 & 300 that are divisible by 7 but not by 63 is %d", sum);
+    printf("Sum of integers between 9 & 300 that are divisible by 7 but not by 63 is %d\n", sum);
     return 0;
 }
