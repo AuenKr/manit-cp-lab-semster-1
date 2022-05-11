48442c0fe7d63d4e26b06cdd030f730d32c79271
AuenKr <103646877+AuenKr@users.noreply.github.com>
2022-05-11 08:41:53 +0530

    Update q11.c

--- assignment2/q11.c
+++ assignment2/q11.c
@@ -11,16 +11,16 @@ int main()
     scanf("%d %d %d %d", &a, &b, &c, &d);
 
     if (a >= b && a >= c && a >= d)
-        printf("%d is greatest", a);
+        printf("%d is greatest\n", a);
 
     else if (b >= c && b >= d)
-        printf("%d is greatest", b);
+        printf("%d is greatest\n", b);
 
     else if (c >= d)
-        printf("%d is greatest", c);
+        printf("%d is greatest\n", c);
 
     else
-        printf("%d is greatest", d);
+        printf("%d is greatest\n", d);
 
     return 0;
 }
