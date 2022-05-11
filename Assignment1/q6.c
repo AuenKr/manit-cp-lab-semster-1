2a683d7873f019549aed82a69c0e8b4a61f53377
AuenKr <103646877+AuenKr@users.noreply.github.com>
2022-05-11 08:38:34 +0530

    Update q6.c

--- Assignment1/q6.c
+++ Assignment1/q6.c
@@ -12,6 +12,6 @@ int main()
     char a;
     printf("Enter a character : ");
     scanf("%c",&a);
-    printf("ASCII value of %c is :%d", a, a);
+    printf("ASCII value of %c is :%d\n", a, a);
     return 0;
 }
